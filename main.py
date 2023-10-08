beb0d2b8bbf6dcab421ca76588a93242f7350bb5
CA1845 <huyunan0815@gmail.com>
2023-10-08 14:06:57 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+# 1.1 path parameters
 @app.get("/items/{item_id}")
-async def read_item(item_id:str):
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+# 1.2 query parameters
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# http://127.0.0.1:8000/items/?skip=0&limit=10
+@app.get("/items")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+# 1.3 optioonal parameters
+@app.get("/itemsid/{item_id}")
+async def read_item(item_id: str, q: str = None):
+    if q:
+        return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
